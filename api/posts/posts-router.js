97d076ee54506a3435009feda2d376f429aadd96
Mina <minasoha@yahoo.com>
2022-01-11 17:18:19 -0500

    added index server and basic posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,18 @@
 // implement your posts router here
+const express = require("express");
+const Posts = require("./posts-model");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+ Posts.find(req.query)
+  .then((post) => {
+   res.status(200).json(post);
+  })
+  .catch((err) => {
+   res
+    .status(500)
+    .json({ message: "The posts information could not be retrieved" });
+  });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,10 @@
 // implement your server here
+const express = require("express");
+const postRouter = require("./posts/posts-router");
+const server = express();
+
+server.use(express.json());
+server.use;
 // require your posts router and connect it here
+
+module.exports = server;
