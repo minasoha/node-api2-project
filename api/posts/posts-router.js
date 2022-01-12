9d345d9512be68dd5e54edf40a4e7a4e2ab361b5
Mina <minasoha@yahoo.com>
2022-01-12 01:51:29 -0500

    finished everything

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -57,4 +57,79 @@ router.post("/", (req, res) => {
    });
  }
 });
+
+router.delete("/:id", async (req, res) => {
+ try {
+  const post = await Posts.findById(req.params.id);
+  if (!post) {
+   res.status(404).json({
+    message: "The post with the specified ID does not exist",
+   });
+  } else {
+   await Posts.remove(req.params.id);
+   res.json(post);
+  }
+ } catch (err) {
+  res.status(500).json({
+   message: "The post could not be removed",
+   err: err.message,
+   stack: err.stack,
+  });
+ }
+});
+router.put("/:id", (req, res) => {
+ const { title, contents } = req.body;
+ if (!title || !contents) {
+  res.status(400).json({
+   message: "Please provide title and contents for the post",
+  });
+ } else {
+  Posts.findById(req.params.id)
+   .then((post) => {
+    if (!post) {
+     res.status(404).json({
+      message: "The post with the specified ID does not exist",
+     });
+    } else {
+     return Posts.update(req.params.id, req.body);
+    }
+   })
+   .then((data) => {
+    if (data) {
+     return Posts.findById(req.params.id);
+    }
+   })
+   .then((post) => {
+    if (post) {
+     res.json(post);
+    }
+   })
+   .catch((err) => {
+    res.status(500).json({
+     message: "The post could not be removed",
+     err: err.message,
+     stack: err.stack,
+    });
+   });
+ }
+});
+router.get("/:id/comments", async (req, res) => {
+ try {
+  const post = await Posts.findById(req.params.id);
+  if (!post) {
+   res.status(404).json({
+    message: "The post with the specified ID does not exist",
+   });
+  } else {
+   const messages = await Posts.findPostComments(req.params.id);
+   res.json(messages);
+  }
+ } catch (err) {
+  res.status(500).json({
+   message: "The comments information could not be retrieved",
+   err: err.message,
+   stack: err.stack,
+  });
+ }
+});
 module.exports = router;
